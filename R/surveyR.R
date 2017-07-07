606fac5e081122cac5159eaf0762f09a220789bc
Kevin Stierhoff <Kevin.Stierhoff@gmail.com>
2017-07-07 13:29:26 -0700

    Added numbers2words function.

--- R/surveyR.R
+++ R/surveyR.R
@@ -210,7 +210,6 @@ xy2latlon <- function(x,y,lat,lon,units="km"){
 #' @param pad Percentage that map boundaries are extended beyond the input data.
 #' @return A data frame containing the range of \code{lat} and \code{lon}.
 #' @export
-# Function for calculating the map boundaries from lat/lon data
 map_bounds <- function(lat,lon,pad = 0.05){
   # configure survey plan map
   # determine the lat/lon to add to the data range to achieve the desired frame
@@ -221,4 +220,60 @@ map_bounds <- function(lat,lon,pad = 0.05){
   range.lon <- c(min(lon)-pad.y,max(lon)+pad.y)
   # Return data frame with lat/lon range
   data.frame(range.lat,range.lon)
+}
+
+#' Convert numbers greater than 9 to words.
+#'
+#' @param x Number to convert to letters
+#' @return If less than 10, a number, else the number spelled-out in words.
+#' @export
+numbers2words <- function(x){
+  ## Function by John Fox found here:
+  ## http://tolstoy.newcastle.edu.au/R/help/05/04/2715.html
+  ## Tweaks by AJH to add commas and "and"
+  helper <- function(x){
+
+    digits <- rev(strsplit(as.character(x), "")[[1]])
+    nDigits <- length(digits)
+    if (nDigits == 1) as.vector(ones[digits])
+    else if (nDigits == 2)
+      if (x <= 19) as.vector(teens[digits[1]])
+    else trim(paste(tens[digits[2]],
+                    Recall(as.numeric(digits[1]))))
+    else if (nDigits == 3) trim(paste(ones[digits[3]], "hundred and",
+                                      Recall(makeNumber(digits[2:1]))))
+    else {
+      nSuffix <- ((nDigits + 2) %/% 3) - 1
+      if (nSuffix > length(suffixes)) stop(paste(x, "is too large!"))
+      trim(paste(Recall(makeNumber(digits[
+        nDigits:(3*nSuffix + 1)])),
+        suffixes[nSuffix],"," ,
+        Recall(makeNumber(digits[(3*nSuffix):1]))))
+    }
+  }
+  trim <- function(text){
+    #Tidy leading/trailing whitespace, space before comma
+    text=gsub("^\ ", "", gsub("\ *$", "", gsub("\ ,",",",text)))
+    #Clear any trailing " and"
+    text=gsub(" and$","",text)
+    #Clear any trailing comma
+    gsub("\ *,$","",text)
+  }
+  makeNumber <- function(...) as.numeric(paste(..., collapse=""))
+  #Disable scientific notation
+  opts <- options(scipen=100)
+  on.exit(options(opts))
+  ones <- c("", "one", "two", "three", "four", "five", "six", "seven",
+            "eight", "nine")
+  names(ones) <- 0:9
+  teens <- c("ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+             "sixteen", " seventeen", "eighteen", "nineteen")
+  names(teens) <- 0:9
+  tens <- c("twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty",
+            "ninety")
+  names(tens) <- 2:9
+  x <- round(x)
+  suffixes <- c("thousand", "million", "billion", "trillion")
+  if (length(x) > 1) return(trim(sapply(x, helper)))
+  helper(x)
 }
